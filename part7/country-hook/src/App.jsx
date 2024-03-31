f3e70c115b9faaf2f30b568172a6e26be197affa
mhbagheri-99 <mhbagheri3@gmail.com>
2024-03-31 16:07:09 +0200

    7.7

--- part7/country-hook/src/App.jsx
+++ part7/country-hook/src/App.jsx
@@ -18,7 +18,19 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+        .then(response => {
+          setCountry({ data: response.data, found: true })
+        })
+        .catch(() => {
+          setCountry({ found: false })
+        })
+    }
+  }
+  , [name])
 
   return country
 }
@@ -38,10 +50,10 @@ const Country = ({ country }) => {
 
   return (
     <div>
-      <h3>{country.data.name} </h3>
-      <div>capital {country.data.capital} </div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital[0]} </div>
       <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <img src={country.data.flags.svg} height='100' alt={`flag of ${country.data.name.common}`}/>  
     </div>
   )
 }
